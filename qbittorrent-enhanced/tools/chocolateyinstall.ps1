6152ac7796189b7b473b4f3ab57d89be62fbf00f
Yato <me@oha.moe>
2020-10-23 12:06:10 +0800

    qbittorrent-enhanced: Update to v4.3.0.10

--- qbittorrent-enhanced/tools/chocolateyinstall.ps1
+++ qbittorrent-enhanced/tools/chocolateyinstall.ps1
@@ -1,24 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.2.5.16/qbittorrent_4.2.5.16_setup.exe'
-$url64 = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.2.5.16/qbittorrent_4.2.5.16_x64_setup.exe'
-$checksum = '11902f421ed125398c8d93cb6c273f9e29d8837d889c6314dca530f09fdd9423'
-$checksum64 = 'b5cf31402a6ea215b5334b082d9bfe0dfd0d105a5da8d3ccd2af0f8d99d3ff22'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName32 = 'qbittorrent_4.3.0.10_setup.exe'
+$fileName64 = 'qbittorrent_4.3.0.10_x64_setup.exe'
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'exe'
-  url            = $url
-  url64bit       = $url64
-
-  softwareName   = 'qbittorrent*'
-
-  checksum       = $checksum
-  checksumType   = 'sha256'
-  checksum64     = $checksum64
-  checksumType64 = 'sha256'
-
-  validExitCodes = @(0, 3010, 1641)
-  silentArgs     = '/S'
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'exe'
+  file         = "$toolsDir\$fileName32"
+  file64       = "$toolsDir\$fileName64"
+  softwareName = 'qbittorrent*'
+  silentArgs   = '/S'
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $packageArgs.file $packageArgs.file64
--- qbittorrent-enhanced/update.ps1
+++ qbittorrent-enhanced/update.ps1
@@ -1,5 +1,9 @@
 $releases = 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases'
 
+function global:au_BeforeUpdate() {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
@@ -11,18 +15,23 @@ function global:au_GetLatest {
     $url64 -match 'qbittorrent_([\d.]+)_x64_setup.exe$'
     $version = $matches[1]
 	
-    return @{ Version = $version; URL = $url; URL64 = $url64 }
+    return @{ Version = $version; URL32 = $url; URL64 = $url64 }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]fileName32\s*=\s*)('.*')" = "`$1'$($Latest.FileName32)'"
+            "(^[$]fileName64\s*=\s*)('.*')" = "`$1'$($Latest.FileName64)'"
+        }
+
+        "tools\verification.txt"      = @{
+            "(?i)(32-Bit.+)\<.*\>"   = "`${1}<$($Latest.URL32)>"
+            "(?i)(64-Bit.+)\<.*\>"   = "`${1}<$($Latest.URL64)>"
+            "(?i)(checksum32:\s+).*" = "`${1}$($Latest.Checksum32)"
+            "(?i)(checksum64:\s+).*" = "`${1}$($Latest.Checksum64)"
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { update }
+if ($MyInvocation.InvocationName -ne '.') { update -ChecksumFor none }
