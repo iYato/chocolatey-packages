887fca579492f0f4d2c44a2952dde11ae816968e
Yato <me@oha.moe>
2020-11-15 09:34:43 +0800

    ventoy: Update to v1.0.28

--- ventoy/tools/chocolateyinstall.ps1
+++ ventoy/tools/chocolateyinstall.ps1
@@ -1,23 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'ventoy-1.0.27-windows.zip'
+$fileName32 = 'ventoy-1.0.28-windows.zip'
 $packageName = $env:ChocolateyPackageName
-$shortcutsPath = Join-Path ([Environment]::GetFolderPath("Programs")) 'Ventoy.lnk'
-$unzipPath = "$Env:LOCALAPPDATA\$packageName"
+$shortcutPath = [Environment]::GetFolderPath("Programs") + '\Ventoy.lnk'
+$unzipLocation = "$Env:LOCALAPPDATA\$packageName"
 $version = $env:ChocolateyPackageVersion
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $unzipPath
+  unzipLocation = $unzipLocation
   file          = "$toolsDir\$fileName32"
-  softwareName  = 'ventoy'
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-Copy-Item -Path "$unzipPath/ventoy-$version/*" -Destination $unzipPath -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item "$unzipPath/ventoy-$version" -Force -Recurse -ErrorAction SilentlyContinue
+Copy-Item -Path "$unzipLocation/ventoy-$version/*" -Destination $unzipLocation -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item "$unzipLocation/ventoy-$version" -Force -Recurse -ErrorAction SilentlyContinue
 
-$exePath = (Get-ChildItem $unzipPath -Filter "Ventoy2Disk.exe" -Recurse).FullName
-Install-ChocolateyShortcut -shortcutFilePath $shortcutsPath -Target "$exePath" -WorkingDirectory $unzipPath
+$exePath = (Get-ChildItem $unzipLocation -Filter "Ventoy2Disk.exe" -Recurse).FullName
+Install-ChocolateyShortcut -shortcutFilePath $shortcutPath -Target "$exePath" -WorkingDirectory $unzipLocation
