5948c80501575e07026354a869008f0dc3682da1
Yato <me@oha.moe>
2020-11-30 14:02:08 +0800

    snipaste: Update to v2.5.5-Beta

--- snipaste/tools/chocolateyuninstall.ps1
+++ snipaste/tools/chocolateyuninstall.ps1
@@ -4,6 +4,4 @@ $shortcutPath = [Environment]::GetFolderPath("Programs") + '\Snipaste.lnk'
 $unzipLocation = $Env:LOCALAPPDATA + "\$env:ChocolateyPackageName"
 
 Remove-Item -Path $shortcutPath -ErrorAction SilentlyContinue
-if (Test-Path $unzipLocation) { 
-    Remove-Item -Path $unzipLocation -Recurse -Force -ErrorAction SilentlyContinue 
-}
+Remove-Item -Path $unzipLocation -Recurse -Force -ErrorAction SilentlyContinue 
