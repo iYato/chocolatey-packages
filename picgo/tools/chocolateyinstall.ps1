4fb4f64fa860111e45faf5094597110cb81ccfb1
Yato <me@oha.moe>
2020-12-20 11:44:22 +0800

    picgo: Update to v2.3.0-beta4

--- picgo/tools/chocolateyinstall.ps1
+++ picgo/tools/chocolateyinstall.ps1
@@ -1,16 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/Molunerfinn/PicGo/releases/download/v2.3.0-beta.3/PicGo-Setup-2.3.0-beta.3.exe'
-$checksum = 'e459ece4ee6d9c9692c2685842df54da4cf045010817c1a0cc651aecb72f73b2'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName32 = 'PicGo-Setup-2.3.0-beta.4.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = $url
   softwareName   = 'PicGo*'
-  checksum       = $checksum
-  checksumType   = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'
+  file           = "$toolsDir\$fileName32"
 }
 
 Install-ChocolateyPackage @packageArgs
