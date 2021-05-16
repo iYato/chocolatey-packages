4fa3eea2ee7edaf777ae10d0071a6c901915747d
Yato <me@oha.moe>
2021-05-16 09:13:40 +0800

    motrix: Update to v1.6.11

--- motrix/tools/chocolateyinstall.ps1
+++ motrix/tools/chocolateyinstall.ps1
@@ -1,19 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/agalwood/Motrix/releases/download/v1.5.15/Motrix-Setup-1.5.15.exe'
-$checksum = 'ea28b37ad26a5cff260fc19365b28ebdaac1179f953d795a6a1ae4326c2aa110'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName32 = 'Motrix-Setup-1.6.11.exe'
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'exe'
-  url            = $url
-
-  softwareName   = 'motrix*'
-
-  checksum       = $checksum
-  checksumType   = 'sha256'
-
-  validExitCodes = @(0, 3010, 1641)
-  silentArgs     = '/S'
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'exe'
+  file         = "$toolsDir\$fileName32"
+  softwareName = 'motrix*'
+  silentArgs   = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
--- motrix/update.ps1
+++ motrix/update.ps1
@@ -1,5 +1,10 @@
 $releases = 'https://github.com/agalwood/Motrix/releases'
 
+function global:au_BeforeUpdate() {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
@@ -9,16 +14,21 @@ function global:au_GetLatest {
     $url -match 'Motrix-Setup-([\d.]+).exe$'
     $version = $matches[1]
 	
-    return @{ Version = $version; URL = $url }
+    return @{ Version = $version; URL32 = $url }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]fileName32\s*=\s*)('.*')" = "`$1'$($Latest.FileName32)'"
+        }
+
+        "tools\verification.txt"      = @{
+            "(?i)(32-Bit.+)\<.*\>"   = "`${1}<$($Latest.URL32)>"
+            "(?i)(checksum32:\s+).*" = "`${1}$($Latest.Checksum32)"
         }
     }
 }
 
-update
+
+if ($MyInvocation.InvocationName -ne '.') { update -ChecksumFor none }
