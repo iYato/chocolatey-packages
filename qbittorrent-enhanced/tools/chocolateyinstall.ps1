33b7bd896525ed54fcf146117533ce915ac52c19
Yato <me@oha.moe>
2021-06-28 13:51:42 +0800

    qbittorrent-enhanced: Update to v4.3.6.10

--- qbittorrent-enhanced/tools/chocolateyinstall.ps1
+++ qbittorrent-enhanced/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'qbittorrent_4.3.5.10_setup.exe'
-$fileName64 = 'qbittorrent_4.3.5.10_x64_setup.exe'
+$fileName32 = 'qbittorrent_4.3.6.10_setup.exe'
+$fileName64 = 'qbittorrent_4.3.6.10_x64_setup.exe'
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
